0056aea3ca7f3a3a471438d82257d936457d38c0
Fiasko <snakeice2000@mail.ru>
2020-10-17 17:07:50 +0300

    happyEnd

--- createF.php
+++ createF.php
@@ -3,6 +3,7 @@
 $login = file_get_contents('php://input');
 $path = getcwd();
 $fullPath = "{$path}/users/{$login}";
+
 if (!file_exists($fullPath)) {
     mkdir($fullPath);
     echo 1;
--- main.js
+++ main.js
@@ -3,7 +3,13 @@ var app = new Vue({
     data() {
         return {
             instruction: false,
-            profile: false,
+            reversePage: false,
+            namePage: "Профили",
+            anotherUser: false,
+            filesAnyUser: [],
+
+            users: [],
+            listProfile: true,
 
             addFile: false,
             status: false,
@@ -24,6 +30,9 @@ var app = new Vue({
             
             newText: '',
             newStatus: false,
+
+            varFilter: 'public',
+            filterFiles: false,
         }
     },
     methods: {
@@ -138,11 +147,19 @@ var app = new Vue({
                 alert("Login field is empty")
             }
         },
+        isEmpty: function (obj) {
+            for (let key in obj) {
+              return false;
+            }
+            return true;
+        },
         getListFileByUser: function() {
             let self = this;
             axios.get(`http://localhost:3000/users?login=${self.userLogin}&role=3`)
             .then(function(response) {
-                self.getFilesById(response.data[0]['id']);
+                if(!self.isEmpty(response.data)) {
+                    self.getFilesById(response.data[0]['id']);
+                }
             })
         },
         getFilesById: function(idUser) {
@@ -150,6 +167,7 @@ var app = new Vue({
             axios.get(`http://localhost:3000/files?id_user=${idUser}`)
             .then(function(response) {
                 self.filesList = response.data;
+                self.onFilter();
             })
         },
         changeInfo: function(idItem, idUser, name) {
@@ -203,6 +221,57 @@ var app = new Vue({
         onDelete: function() {
             this.delete = !this.delete;
         },
-        
+        changePage: function() {
+            this.reversePage = !this.reversePage;
+            this.namePage = this.reversePage ? "Файлы" : "Профили";
+            let self = this;
+            axios.get('http://localhost:3000/users?role=3')
+            .then(function(response) {
+                self.users = response.data;
+            })
+        },
+        onProfile: function(userID) {
+            let self = this;
+            if(this.reversePage) {
+                self.anotherUser = true;
+                axios.get(`http://localhost:3000/files?id_user=${userID}&status_file=public`)
+                .then(function(response) {
+                    self.filesAnyUser = response.data;
+                })
+            }
+        },
+        loadFile: async function(fileName, userID) {
+            let userName = '';
+            await axios.get(`http://localhost:3000/users/${userID}`)
+            .then((response) => {
+                userName = response.data['login'];
+			})
+            await axios.get(`http://localhost/lb3/users/${userName}/${fileName}`, 
+            {
+                responseType: 'blob'
+            })
+            .then((response) => {
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${fileName}`);
+                document.body.appendChild(link);
+                link.click();
+			})
+        },
+        onFilter: function() {
+            let self = this;
+            this.filterFiles = !this.filterFiles;
+            this.varFilter = this.filterFiles ? "public" : "private";
+            if(this.filterFiles) {
+                self.filesList.sort(function(a, b) {
+                    return a.status_file > b.status_file ? 1 : -1;
+                });
+            } else {
+                self.filesList.sort(function(a, b) {
+                    return b.status_file > a.status_file ? 1 : -1;
+                });
+            }
+        }
     }
 })
